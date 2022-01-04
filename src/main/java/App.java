455962ace4dc6b10a994b6c15efc23d25a6c9d53
IrinaRazumova <irina.v.razumova@gmail.com>
2022-01-04 13:38:25 +0300

    1.Создан POJO-класс Cat. 2.В классе AppConfig, по аналогии с @Bean(name="helloworld"), создан бин с именем “cat”. 3.Бин "Cat" настроен так, чтобы он создавался новым при каждом запросе. 4.В классе App дважды вызваны бины helloWorld и cat. При сравнении  пары helloWorld возвращается true, пары cat - false.

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -7,6 +7,23 @@ public class App {
                 new AnnotationConfigApplicationContext(AppConfig.class);
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
+        HelloWorld bean3 =//
+                (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+        System.out.println(bean3.getMessage());
+
+        boolean comparisonHello = bean == bean3;
+        System.out.println(comparisonHello);
+
+        Cat bean2 =
+                (Cat) applicationContext.getBean("cat");
+        Cat bean4 =
+                (Cat) applicationContext.getBean("cat");
+        System.out.println(bean2.getMessage());
+        System.out.println(bean2.getMessage());
+
+        boolean comparisonCat = bean2 == bean4;
+        System.out.println(comparisonCat);
+
     }
 }
